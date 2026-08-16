b6fc60405fa59801c6482a61f01908d1153b7a79
Dipti17Bhawar <diptibhawar@gmail.com>
2026-08-16 11:55:55 +0530

    Fix ES module server setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -18,7 +20,6 @@ app.get("/", (req, res) => {
 // Port
 const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
