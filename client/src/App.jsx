995867fe3f863b30edbdd6b1fab976cfeef37445
Dipti17Bhawar <diptibhawar@gmail.com>
2026-09-24 13:45:43 +0530

    change code of subject for add resources

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -10,9 +10,15 @@ import Register from "./views/Register/Register";
 import Dashboard from "./views/Dashboard/Dashboard";
 import BranchSubjects from "./views/BranchSubjects/BranchSubjects";
 import Subject from "./views/Subject/Subject";
+import Resources from "./views/Resources/Resources";
 import About from "./views/About/About";
 import Reviews from "./views/Reviews/Reviews";
 import NotFound from "./views/NotFound/NotFound";
+
+
+// ========================================
+// LAYOUT
+// ========================================
 
 function Layout({ children }) {
   return (
@@ -28,46 +34,91 @@ function Layout({ children }) {
   );
 }
 
+
+// ========================================
+// APP
+// ========================================
+
 export default function App() {
   return (
     <Layout>
+
       <Routes>
 
-        {/* PUBLIC */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/reviews" element={<Reviews />} />
+        {/* ==================================
+            PUBLIC ROUTES
+        ================================== */}
 
-        {/* PROTECTED */}
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/about"
+          element={<About />}
+        />
+
+        <Route
+          path="/reviews"
+          element={<Reviews />}
+        />
+
+
+        {/* ==================================
+            PROTECTED ROUTES
+        ================================== */}
+
         <Route element={<ProtectedRoute />}>
 
+          {/* Dashboard */}
           <Route
             path="/dashboard"
             element={<Dashboard />}
           />
 
+          {/* Branch Subjects */}
           <Route
             path="/branch/:code"
             element={<BranchSubjects />}
           />
 
-          {/* IMPORTANT */}
+          {/* Subject Details */}
           <Route
             path="/subject/:subjectId"
             element={<Subject />}
           />
 
+          {/* Subject Resources */}
+          <Route
+            path="/subject/:subjectId/resources"
+            element={<Resources />}
+          />
+
         </Route>
 
-        {/* 404 */}
+
+        {/* ==================================
+            404 PAGE
+        ================================== */}
+
         <Route
           path="*"
           element={<NotFound />}
         />
 
       </Routes>
+
     </Layout>
   );
 }
