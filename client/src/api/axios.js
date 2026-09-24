9a307c6291735c8d7af10b1a4498b449867a3dae
Dipti17Bhawar <diptibhawar@gmail.com>
2026-09-24 11:35:53 +0530

    add baseurl

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-learning-platform-for-engineers-1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
