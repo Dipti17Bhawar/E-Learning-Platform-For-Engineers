dd5f26410b51bd5f29833f011fce0272fe290131
Dipti17Bhawar <diptibhawar@gmail.com>
2026-09-24 11:39:03 +0530

    change code for axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-learning-platform-for-engineers-1.onrender.com",
+  baseURL: "https://e-learning-platform-for-engineers-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,17 +9,14 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-
     const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   },
-
   (error) => {
     return Promise.reject(error);
   }
